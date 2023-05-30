ee87f7376767ecfc302ead18e97fd528b9ce9b29
vinod <borse002@gmail.com>
2023-05-30 09:29:45 +0530

    day-12 code

---
+++ Day-12/sample.js
@@ -0,0 +1,11 @@
+// console.log("YOUR RESUT IS = ", 3+30);
+
+function add(a,b) {
+    if(a+b >= 10) {
+        console.log("YOU ARE PASS");
+    } else {
+        console.log("YOU ARE FAIL");
+    }
+}
+
+add(2,14);
