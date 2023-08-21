182a3f7ca21ba667840aceb20f32dd2b56642d7f
Vinodkumar Borse <borse002@gmail.com>
2023-08-21 10:40:45 +0530

    Add files via upload

---
+++ User-ui/user.js
@@ -0,0 +1,32 @@
+
+$('#clk-btn').click(function(){
+    getAllUsers();
+})
+
+async function getAllUsers() {
+    const url = "http://localhost:8082/get-all";
+    const result = await fetch(url, {method: 'GET'});
+    const finalResult = await result.json();
+
+    console.log(finalResult);
+
+}
+
+
+$('#user-btn').click(function() {  
+    getUserById();
+})
+
+
+async function getUserById() {
+
+    const userId = $('#user-id').val();
+
+    const url = 'http://localhost:8082/get-user?id='+ userId;
+    const res = await fetch(url, {method: 'GET'});
+    const finalRes = await res.json();
+    console.log(finalRes);
+
+    $('#result-data').html('<h1>'+ finalRes.firstName + ' '+ finalRes.lastName + '</h1>');
+
+}
